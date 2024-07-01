c37124143ac05ae754f21dce4237db21b67035f4
suryaanshchawla <suryaanshchawla@gmail.com>
2024-07-01 18:43:26 +0530

    added lcount, wcount, bcount, reached step4

--- ccwc.py
+++ ccwc.py
@@ -1,16 +1,55 @@
+def takefilepath():
+    file_path=str(input("enter file path please: "))
+
+
 def bytesinfile(): # show the bytes of a file
-    file_path=str(input("enter the file path please: "))
+    takefilepath()
     try:
         with open(file_path, 'rb') as file:
-            print(len(file.read()))
+            return len(file.read())
     except FileNotFoundError:
         print("Enter the correct path please :) ")
         return None
 
 print("Namaste, this is my implimentation of the wc program in linux in a more user friendly way.")
 
-function=input(""" What do you want to do? \n1. Find out number of bytes (type 'c') """)
+function=input(""" What do you want to do? \n1. Find out number of bytes (type 'c') \n2. Find out number of lines (type 'l')\n3. Find out number of words (type 'w')\n""")
     
+
+  # number of lines
+def numoflines():
+    takefilepath()
+    try:
+        with open(file_path, 'rb') as file:
+            lcount=sum(1 for line in file)
+            return lcount
+            
+    except FileNotFoundError:
+        print("Enter the correct path please :) ")
+        return None
+
+# number of words
+def numofwords():
+    takefilepath()
+    try:
+        with open(file_path, 'rb') as file:
+            wcount=len((file.read()).split())
+            return wcount
+            
+    except FileNotFoundError:
+        print("Enter the correct path please :) ")
+        return None
+
+
+
+
+
+
+
 if function=='c':
-    bytesinfile()
+    print(bytesinfile())
+elif function == 'l':
+    print(numoflines())
+elif function == 'w':
+    print(numofwords())
 
