233963619be07529fd566294f8d3e92ddaae76c1
suryaanshchawla <suryaanshchawla@gmail.com>
2024-07-01 14:21:07 +0530

    implimneted bytesinfile

--- ccwc.py
+++ ccwc.py
@@ -1,5 +1,16 @@
-# show the bytes of a file
+def bytesinfile(): # show the bytes of a file
+    file_path=str(input("enter the file path please: "))
+    try:
+        with open(file_path, 'rb') as file:
+            print(len(file.read()))
+    except FileNotFoundError:
+        print("Enter the correct path please :) ")
+        return None
 
-file_path = 'home/suryaansh/Development/my-wc-for-linux/test.txt' 
-file = open(file_path, 'rb')
-print(file)
+print("Namaste, this is my implimentation of the wc program in linux in a more user friendly way.")
+
+function=input(""" What do you want to do? \n1. Find out number of bytes (type 'c') """)
+    
+if function=='c':
+    bytesinfile()
+
